172c70a1923319483541465c83e97268cd37b775
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-02-14 15:20:44 -0500

    add old drawing logic and extra face landmarks

---
+++ bin.py
@@ -0,0 +1,32 @@
+########### OLD DRAWING LOGIC ###########
+# mp_drawing.draw_landmarks(
+#     image=image,
+#     landmark_list=face_landmarks,
+#     connections=mp_face_mesh.FACEMESH_TESSELATION,
+#     landmark_drawing_spec=None,
+#     connection_drawing_spec=mp_drawing_styles
+#     .get_default_face_mesh_tesselation_style())
+# mp_drawing.draw_landmarks(
+#     image=image,
+#     landmark_list=face_landmarks,
+#     connections=mp_face_mesh.FACEMESH_CONTOURS,
+#     landmark_drawing_spec=None,
+#     connection_drawing_spec=mp_drawing_styles
+#     .get_default_face_mesh_contours_style())
+# mp_drawing.draw_landmarks(
+#     image=image,
+#     landmark_list=face_landmarks,
+#     connections=mp_face_mesh.FACEMESH_IRISES,
+#     landmark_drawing_spec=None,
+#     connection_drawing_spec=mp_drawing_styles
+#     .get_default_face_mesh_iris_connections_style())
+        
+full_outer_top = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
+full_inner_top = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
+full_inner_bottom = [78, 88, 87, 14, 317, 318, 308]
+full_outer_bottom = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291]
+full_left_eyebrow = [70, 63, 105, 66, 107]
+full_right_eyebrow = [336, 296, 334, 293, 300]
+full_upper_lip_nose = [206, 203, 167, 164, 393, 426, 423]
+full_left_eye = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398]
+full_right_eye = [33, 161, 246, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160]
